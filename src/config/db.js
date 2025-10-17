37933eef52838f256299a8168e2df1a5a8c87664
ismax26 <ismael.zaidi@gmail.com>
2025-10-17 10:54:20 +0200

    modif code

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,20 @@
-export const tasks = [];
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+// Création du pool de connexions PostgreSQL
+export const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+});
+
+// Vérification de la connexion
+pool.connect()
+  .then(() => console.log("✅ Connecté à PostgreSQL"))
+  .catch((err) => console.error("❌ Erreur de connexion à PostgreSQL :", err));
